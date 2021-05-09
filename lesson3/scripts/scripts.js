a32c04e7b51f7908fd7e1b75d25b729605d11071
jmerk21 <jmerkley21@gmail.com>
2021-05-08 19:04:40 -0600

    Added weather plan site

--- lesson3/scripts/scripts.js
+++ lesson3/scripts/scripts.js
@@ -5,12 +5,3 @@ window.addEventListener('load', (event)=>{
     const cry = document.querySelector('#copyrightyear');
     cry.textContent = new Date().getFullYear();
 })
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
